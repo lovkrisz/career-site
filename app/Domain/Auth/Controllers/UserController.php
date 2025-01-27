0599ca22d81fd5250b3ef2abba97961600db1efa
lovkrisz <72939589+lovkrisz@users.noreply.github.com>
2025-01-27 21:51:34 +0100

    Initial

---
+++ app/Domain/Auth/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Auth\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
